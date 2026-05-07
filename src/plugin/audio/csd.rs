e7a8235820ab7125b215573997f29a8641f1418c
anton-k <anton.kholomiov@gmail.com>
2026-05-07 15:15:43 +0300

    Refactor csd binding

--- src/plugin/audio/csd.rs
+++ src/plugin/audio/csd.rs
@@ -10,7 +10,7 @@ pub fn init_csound(sample_rate: usize) -> Csound {
     csound
         .set_option(format!("-n -d -+rtmidi=NULL -M0 -sr {}", sample_rate).as_str())
         .expect("Failed to set sample rate");
-    csound.compile_csd(REVERB_CSD_FILE, 1, 1).unwrap();
+    csound.compile_csd_from_str(REVERB_CSD_FILE, true).unwrap();
     csound.start().expect("Failed to start Csound");
     csound
 }
@@ -18,10 +18,14 @@ pub fn init_csound(sample_rate: usize) -> Csound {
 #[repr(C)]
 struct CsoundPtr;
 
+#[allow(dead_code)]
 pub struct Csound {
     lib: libloading::Library,
     csound_ptr: *mut CsoundPtr,
 
+    csound_set_option: unsafe extern "C" fn(*mut CsoundPtr, *const c_char) -> i32,
+    csound_compile_csd: unsafe extern "C" fn(*mut CsoundPtr, *const c_char, i32, i32) -> i32,
+    csound_start: unsafe extern "C" fn(*mut CsoundPtr) -> i32,
     csound_perform_ksmps: unsafe extern "C" fn(*mut CsoundPtr) -> i32,
     csound_set_control_channel: unsafe extern "C" fn(*mut CsoundPtr, *const c_char, f64) -> i32,
     csound_get_control_channel:
@@ -41,12 +45,19 @@ impl Csound {
             > = lib.get(b"csoundCreate")?;
 
             let csound_ptr = csound_create(null(), null());
+            let csound_set_option = *lib.get(b"csoundSetOption")?;
+            let csound_compile_csd = *lib.get(b"csoundCompileCSD")?;
+            let csound_start = *lib.get(b"csoundStart")?;
+
             let csound_perform_ksmps = *lib.get(b"csoundPerformKsmps")?;
             let csound_set_control_channel = *lib.get(b"csoundSetControlChannel")?;
             let csound_get_control_channel = *lib.get(b"csoundGetControlChannel")?;
             Ok(Self {
                 lib,
                 csound_ptr,
+                csound_set_option,
+                csound_compile_csd,
+                csound_start,
                 csound_perform_ksmps,
                 csound_set_control_channel,
                 csound_get_control_channel,
@@ -55,23 +66,29 @@ impl Csound {
         .map_err(CsdError)
     }
 
-    pub fn set_option(&mut self, flags: &str) -> Result<(), CsdError> {
+    pub fn set_option(&mut self, flags: &str) -> CsdResult<()> {
         unsafe {
-            let csound_set_option: libloading::Symbol<
-                unsafe extern "C" fn(*mut CsoundPtr, *const c_char) -> i32,
-            > = self.lib.get(b"csoundSetOption")?;
-            csound_set_option(self.csound_ptr, flags.as_ptr() as *const c_char);
+            (self.csound_set_option)(self.csound_ptr, flags.as_ptr() as *const c_char);
             Ok(())
         }
         .map_err(CsdError)
     }
+    pub fn compile_csd_from_str(&mut self, csd: &str, is_async: bool) -> Result<(), CsdError> {
+        self.compile_csd_low_level(csd, 1, if is_async { 1 } else { 0 })
+    }
 
-    pub fn compile_csd(&mut self, csd: &str, mode: i32, is_async: i32) -> Result<(), CsdError> {
+    pub fn compile_csd_from_file(&mut self, csd: &str, is_async: bool) -> Result<(), CsdError> {
+        self.compile_csd_low_level(csd, 0, if is_async { 1 } else { 0 })
+    }
+
+    fn compile_csd_low_level(
+        &mut self,
+        csd: &str,
+        mode: i32,
+        is_async: i32,
+    ) -> Result<(), CsdError> {
         unsafe {
-            let csound_compile_csd: libloading::Symbol<
-                unsafe extern "C" fn(*mut CsoundPtr, *const c_char, i32, i32) -> i32,
-            > = self.lib.get(b"csoundCompileCSD")?;
-            csound_compile_csd(
+            (self.csound_compile_csd)(
                 self.csound_ptr,
                 csd.as_ptr() as *const c_char,
                 mode,
@@ -84,9 +101,7 @@ impl Csound {
 
     pub fn start(&mut self) -> CsdResult<()> {
         unsafe {
-            let csound_start: libloading::Symbol<unsafe extern "C" fn(*mut CsoundPtr) -> i32> =
-                self.lib.get(b"csoundStart")?;
-            csound_start(self.csound_ptr);
+            (self.csound_start)(self.csound_ptr);
             Ok(())
         }
         .map_err(CsdError)
