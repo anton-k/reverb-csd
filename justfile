b164831611a85f44748a047b0aab2e6374b237a1
anton-k <anton.kholomiov@gmail.com>
2026-05-07 20:04:14 +0300

    Adds moere csound bindings

--- justfile
+++ justfile
@@ -4,7 +4,7 @@ build:
 run:
     cargo build --release
     cargo bundle target/release/libreverb_csd.so
-    # cp -r target/release/ReverbCsd.vst3/ ~/music/vst/
+    cp -r target/release/ReverbCsd.vst3/ ~/music/vst/
     # cp target/release/ReverbCsd.clap ~/music/clap/
     # cp target/release/libreverb_csd.so target/release/ReverbCsd.clap 
     clack-host-cpal --file-path target/release/ReverbCsd.clap 
--- src/plugin/audio/csd.rs
+++ src/plugin/audio/csd.rs
@@ -2,7 +2,10 @@
 static REVERB_CSD_FILE: &str = include_str!("./reverb.csd");
 use core::ffi::c_void;
 use std::path::Path;
-use std::{ffi::c_char, ffi::c_int, ptr::null};
+use std::{
+    ffi::{c_char, c_double, c_int},
+    ptr::null,
+};
 
 pub fn init_csound(sample_rate: usize) -> Csound {
     let mut csound =
@@ -26,10 +29,20 @@ pub struct Csound {
     csound_set_option: unsafe extern "C" fn(*mut CsoundPtr, *const c_char) -> i32,
     csound_compile_csd: unsafe extern "C" fn(*mut CsoundPtr, *const c_char, i32, i32) -> i32,
     csound_start: unsafe extern "C" fn(*mut CsoundPtr) -> i32,
+    csound_reset: unsafe extern "C" fn(*mut CsoundPtr) -> c_void,
     csound_perform_ksmps: unsafe extern "C" fn(*mut CsoundPtr) -> i32,
     csound_set_control_channel: unsafe extern "C" fn(*mut CsoundPtr, *const c_char, f64) -> i32,
     csound_get_control_channel:
-        unsafe extern "C" fn(*mut CsoundPtr, *const c_char, *mut c_int) -> f64,
+        unsafe extern "C" fn(*const CsoundPtr, *const c_char, *mut c_int) -> f64,
+    csound_get_ksmps: unsafe extern "C" fn(*const CsoundPtr) -> u32,
+    csound_get_spin: unsafe extern "C" fn(*mut CsoundPtr) -> *mut c_double,
+    csound_get_spout: unsafe extern "C" fn(*const CsoundPtr) -> *mut c_double,
+    csound_get_channels: unsafe extern "C" fn(*const CsoundPtr, i32) -> u32,
+    csound_get_sr: unsafe extern "C" fn(*const CsoundPtr) -> u32,
+    csound_get_kr: unsafe extern "C" fn(*const CsoundPtr) -> u32,
+    csound_get_0dbfs: unsafe extern "C" fn(*const CsoundPtr) -> f64,
+    csound_destroy: unsafe extern "C" fn(*mut CsoundPtr) -> c_void,
+    csound_get_current_time_samples: unsafe extern "C" fn(*const CsoundPtr) -> i64,
 }
 
 unsafe impl Send for Csound {}
@@ -48,19 +61,39 @@ impl Csound {
             let csound_set_option = *lib.get(b"csoundSetOption")?;
             let csound_compile_csd = *lib.get(b"csoundCompileCSD")?;
             let csound_start = *lib.get(b"csoundStart")?;
+            let csound_reset = *lib.get(b"csoundReset")?;
+            let csound_destroy = *lib.get(b"csoundDestroy")?;
 
             let csound_perform_ksmps = *lib.get(b"csoundPerformKsmps")?;
             let csound_set_control_channel = *lib.get(b"csoundSetControlChannel")?;
             let csound_get_control_channel = *lib.get(b"csoundGetControlChannel")?;
+            let csound_get_ksmps = *lib.get(b"csoundGetKsmps")?;
+            let csound_get_spin = *lib.get(b"csoundGetSpin")?;
+            let csound_get_spout = *lib.get(b"csoundGetSpout")?;
+            let csound_get_channels = *lib.get(b"csoundGetChannels")?;
+            let csound_get_sr = *lib.get(b"csoundGetSr")?;
+            let csound_get_kr = *lib.get(b"csoundGetKr")?;
+            let csound_get_0dbfs = *lib.get(b"csoundGet0dBFS")?;
+            let csound_get_current_time_samples = *lib.get(b"csoundGetCurrentTimeSamples")?;
             Ok(Self {
                 lib,
                 csound_ptr,
                 csound_set_option,
                 csound_compile_csd,
                 csound_start,
+                csound_reset,
+                csound_destroy,
                 csound_perform_ksmps,
                 csound_set_control_channel,
                 csound_get_control_channel,
+                csound_get_ksmps,
+                csound_get_spin,
+                csound_get_spout,
+                csound_get_channels,
+                csound_get_sr,
+                csound_get_kr,
+                csound_get_current_time_samples,
+                csound_get_0dbfs,
             })
         }
         .map_err(CsdError)
@@ -107,6 +140,14 @@ impl Csound {
         .map_err(CsdError)
     }
 
+    pub fn reset(&mut self) -> CsdResult<()> {
+        unsafe {
+            (self.csound_reset)(self.csound_ptr);
+            Ok(())
+        }
+        .map_err(CsdError)
+    }
+
     pub fn set_control_channel(&mut self, channel_name: &str, value: f64) -> Result<i32, CsdError> {
         unsafe {
             let res = (self.csound_set_control_channel)(
@@ -119,7 +160,7 @@ impl Csound {
         .map_err(CsdError)
     }
 
-    pub fn get_control_channel(&mut self, channel_name: &str) -> Result<f64, CsdError> {
+    pub fn get_control_channel(&self, channel_name: &str) -> Result<f64, CsdError> {
         unsafe {
             let err_value = 0;
             let res = (self.csound_get_control_channel)(
@@ -138,6 +179,174 @@ impl Csound {
             Ok(res)
         }
         .map_err(CsdError)
+    }
+
+    pub fn get_ksmps(&self) -> CsdResult<u32> {
+        unsafe {
+            let res = (self.csound_get_ksmps)(self.csound_ptr);
+            Ok(res)
+        }
+        .map_err(CsdError)
+    }
+
+    pub fn get_sr(&self) -> CsdResult<u32> {
+        unsafe {
+            let res = (self.csound_get_sr)(self.csound_ptr);
+            Ok(res)
+        }
+        .map_err(CsdError)
+    }
+
+    pub fn get_kr(&self) -> CsdResult<u32> {
+        unsafe {
+            let res = (self.csound_get_kr)(self.csound_ptr);
+            Ok(res)
+        }
+        .map_err(CsdError)
+    }
+
+    pub fn get_0dbfs(&self) -> CsdResult<f64> {
+        unsafe {
+            let res = (self.csound_get_0dbfs)(self.csound_ptr);
+            Ok(res)
+        }
+        .map_err(CsdError)
+    }
+
+    pub fn get_current_time_samples(&self) -> CsdResult<i64> {
+        unsafe {
+            let res = (self.csound_get_current_time_samples)(self.csound_ptr);
+            Ok(res)
+        }
+        .map_err(CsdError)
+    }
+
+    pub fn get_channels(&self, is_input: bool) -> CsdResult<u32> {
+        unsafe {
+            let res = (self.csound_get_channels)(self.csound_ptr, is_input as i32);
+            Ok(res)
+        }
+        .map_err(CsdError)
+    }
+
+    pub fn get_input_len(&self) -> CsdResult<u32> {
+        self.get_channels(true)
+    }
+    pub fn get_output_len(&self) -> CsdResult<u32> {
+        self.get_channels(false)
+    }
+
+    pub fn get_spin<'a>(&mut self) -> CsdResult<&'a mut [f64]> {
+        unsafe {
+            let res = (self.csound_get_spin)(self.csound_ptr);
+            let ksmps = self.get_ksmps()?;
+            let num_of_inputs = self.get_input_len()?;
+            Ok(std::slice::from_raw_parts_mut(
+                res,
+                (ksmps * num_of_inputs) as usize,
+            ))
+        }
+        .map_err(CsdError)
+    }
+
+    pub fn get_spout<'a>(&self) -> CsdResult<&'a [f64]> {
+        unsafe {
+            let res = (self.csound_get_spout)(self.csound_ptr);
+            let ksmps = self.get_ksmps()?;
+            let num_of_outputs = self.get_output_len()?;
+            Ok(std::slice::from_raw_parts(
+                res,
+                (ksmps * num_of_outputs) as usize,
+            ))
+        }
+        .map_err(CsdError)
+    }
+
+    pub fn destroy(&mut self) -> CsdResult<()> {
+        unsafe {
+            let _res = (self.csound_destroy)(self.csound_ptr);
+            Ok(())
+        }
+        .map_err(CsdError)
+    }
+
+    // unsafe and fast variants with no result/error wrappers
+
+    pub fn get_ksmps_unsafe(&self) -> u32 {
+        unsafe { (self.csound_get_ksmps)(self.csound_ptr) }
+    }
+
+    pub fn get_sr_unsafe(&self) -> u32 {
+        unsafe { (self.csound_get_sr)(self.csound_ptr) }
+    }
+
+    pub fn get_kr_unsafe(&self) -> u32 {
+        unsafe { (self.csound_get_kr)(self.csound_ptr) }
+    }
+
+    pub fn get_0dbfs_unsafe(&self) -> f64 {
+        unsafe { (self.csound_get_0dbfs)(self.csound_ptr) }
+    }
+
+    pub fn get_channels_unsafe(&self, is_input: bool) -> u32 {
+        unsafe { (self.csound_get_channels)(self.csound_ptr, is_input as i32) }
+    }
+
+    pub fn get_input_len_unsafe(&self) -> u32 {
+        self.get_channels_unsafe(true)
+    }
+    pub fn get_output_len_unsafe(&self) -> u32 {
+        self.get_channels_unsafe(false)
+    }
+
+    pub fn get_spin_unsafe<'a>(&mut self) -> &'a mut [f64] {
+        unsafe {
+            let res = (self.csound_get_spin)(self.csound_ptr);
+            let ksmps = self.get_ksmps_unsafe();
+            let num_of_inputs = self.get_input_len_unsafe();
+            std::slice::from_raw_parts_mut(res, (ksmps * num_of_inputs) as usize)
+        }
+    }
+
+    pub fn get_spout_unsafe<'a>(&self) -> &'a [f64] {
+        unsafe {
+            let res = (self.csound_get_spout)(self.csound_ptr);
+            let ksmps = self.get_ksmps_unsafe();
+            let num_of_outputs = self.get_output_len_unsafe();
+            std::slice::from_raw_parts(res, (ksmps * num_of_outputs) as usize)
+        }
+    }
+
+    pub fn perform_ksmps_unsafe(&mut self) -> i32 {
+        unsafe { (self.csound_perform_ksmps)(self.csound_ptr) }
+    }
+
+    pub fn set_control_channel_unsafe(&mut self, channel_name: &str, value: f64) -> i32 {
+        unsafe {
+            (self.csound_set_control_channel)(
+                self.csound_ptr,
+                channel_name.as_ptr() as *const c_char,
+                value,
+            )
+        }
+    }
+
+    // ignores errors
+    pub fn get_control_channel_unsafe(&self, channel_name: &str) -> f64 {
+        unsafe {
+            let err_value = 0;
+            (self.csound_get_control_channel)(
+                self.csound_ptr,
+                channel_name.as_ptr() as *const c_char,
+                &err_value as *const i32 as *mut c_int,
+            )
+        }
+    }
+}
+
+impl Drop for Csound {
+    fn drop(&mut self) {
+        let _ = self.destroy();
     }
 }
 
